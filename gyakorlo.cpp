00315ef1e2ca3b9803f343380914c67a056cf0b5
Norbert Sana <neilus@inf.elte.hu>
2012-09-22 16:19:21 +0200

    csucshasonlitas
    graf csucsokat hasonlitom ossze, probabol, kicsit erdekes

--- gyakorlo.cpp
+++ gyakorlo.cpp
@@ -4,30 +4,28 @@
 using namespace std;
 using namespace lemon;
 
+int hasNode(const ListDigraph &miben,const ListDigraph::Node &mi){
+	int talalt=0;
+	for(ListDigraph::NodeIt i(miben); i != INVALID && !talalt; ++i)
+		talalt = (mi == i);
+
+	return talalt;
+
+}
 int main(){
 	ListDigraph g;
-
+	ListDigraph d;
   ListDigraph::Node x = g.addNode();
   ListDigraph::Node y = g.addNode();
   ListDigraph::Node z = g.addNode();
-/*
-  g.addArc(x,y);
-  g.addArc(y,z);
-  g.addArc(z,x);
-  */
-  ListDigraph::Arc arc = g.addArc(x,z);
-  ListDigraph::Arc parallel = g.addArc(x,y);
-  ListDigraph::Arc loop = g.addArc(x,x);
-  cout << endl;
-  if (g.source(parallel) == g.target(parallel))
-    std::cout << "This is a loop arc" << std::endl;
-  else
-  	cout << "nem hurokel" << endl;
 
-  cout << endl;
-  for( ListDigraph::NodeIt i(g); i != INVALID; ++i)
-  	cout <<"\t" << g.id(i);
-  cout << endl;
-  
+  ListDigraph::Node w;// = d.addNode();
+  g.addArc(x,y);
+  g.addArc(x,z);
+  g.addArc(y,z);
+  cout << (w == x) << endl
+  	<< (w == y) << endl
+  	<< (w == z) <<endl;
+
 return 0;
 }

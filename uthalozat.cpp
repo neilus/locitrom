0c9a4206704265731ee6333d40a9f44c26388b12
Norbert Sana <sananorbert@gmail.com>
2012-09-24 01:46:17 +0200

    sikerult valami...

--- uthalozat.cpp
+++ uthalozat.cpp
@@ -11,20 +11,34 @@ using namespace lemon;
 int main()
 {
  ListGraph g;
- ListGraph::NodeMap<int> lat(g);
- ListGraph::NodeMap<int> lon(g);
  ListGraph::Node nodes;
+ ListGraph::Arc	 arcs;
+ ListGraph::NodeMap<int> label(g);
+ ListGraph::NodeMap<double> lat(g);
+ ListGraph::NodeMap<double> lon(g);
  ListGraph::ArcMap<int> length(g);
  ListGraph::ArcMap<int> maxspeed(g);
 
  graphReader(g,"hun.lgf")
+	//.arcMap("length",length)
+	//.arcMap("maxspeed",maxspeed)
+	.nodeMap("label",label)
 	.nodeMap("lat",lat)
 	.nodeMap("lon",lon)
-	.arcMap("lengh",length)
-	.arcMap("mazspeed",maxspeed)
+// valamiert az elek hossza es a sebessegkorlatot 
+// nem tudom igy beolvasni, otlet? hogy kene maskepp?
+	//.edgeMap("length",length)
+	//.edgeMap("maxspeed",maxspeed)
+	.run();
+ 
+ std::cerr << "\n\tA beolvasas lefutott...\n";
+
+ graphWriter(g)
+	.nodeMap("label",label)
+	.nodeMap("lat",lat)
+	.nodeMap("lon",lon)
 	.run();
 
- int c;
- std::cin >> c;
+ std::cerr << "\n kesz vagyok\n";
  return 0;
 }

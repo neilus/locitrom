2dddc1a81b85df70cebcc99cad129cd4cd98771b
Norbert Sana <neilus@inf.elte.hu>
2012-09-30 06:11:06 +0200

    Asszem most mar jo lesz...

--- uthalozat.cpp
+++ uthalozat.cpp
@@ -20,9 +20,12 @@ int main(int argc, char*argv[])
  ListGraph::EdgeMap<int> length(g);
  ListGraph::EdgeMap<int> maxspeed(g);
 
+ ListGraph::NodeMap<bool> visited(g);
 
+ string filename = ( (argc != 2)?"hun.lgf":argv[1] )  ;
+ cerr << "Start to process file:  "<< filename << endl;
  try {
-	 graphReader(g, ( (argc != 2)?"hun.lgf":argv[1] ) )
+	 graphReader(g, filename.c_str())
 		.edgeMap("length",length)
 		.edgeMap("maxspeed",maxspeed)
 		.nodeMap("label",label)
@@ -34,37 +37,41 @@ int main(int argc, char*argv[])
     return -1;
   	}
  
- cerr << "\n\tA beolvasas lefutott...\n";
- cerr << "A digraph is read from 'hun.lgf'." << endl;
- cerr << "Number of nodes: " << countNodes(g) << endl;
- cerr << "Number of arcs: " << countArcs(g) << endl;
-
- cerr << "We can write it to the standard output:" << endl;
- //SmartDigraph d(g);
+ cerr << "\n\tthe graph has been read from the file...\n";
+ // adding a 1-node island in to the graph we just read ... for fun and control
+ // ListGraph::Node island = g.addNode();
+ int SumNodes = countNodes(g);
+// cerr << "Number of arcs: " << countArcs(g) << endl;
+// cerr << "\tNumber of nodes: \t" << SumNodes << endl;
+// for (ListGraph::NodeIt i(g); i != INVALID; ++i)
+//	visited[i]=false;
+ 
+ int reached;
+ int unreached = SumNodes;
  Dfs<ListGraph> dfs(g);
- //Dfs::ReachedMap<ListGraph> reached(dfs);
- // tree.reachedMap();
- dfs.run();
- int reached = 0;
- for (ListGraph::NodeIt i(g); i != INVALID; ++i){
- 	if( dfs.reached(i) ) {
-		reached ++;
-//		cerr << g.id(i) << "\t";
-	}
- }
-
- cerr << "\nWe have reached " << reached << " nodes during DFS" << endl;
-//  countNodes( tree.predMap() );
-
-
-//  graphWriter(tree)
-// 	.edgeMap("length",length)
-// 	.edgeMap("maxspeed",maxspeed)
-// 	.nodeMap("label",label)
-// 	.nodeMap("lat",lat)
-// 	.nodeMap("lon",lon)
-// 	.run();
-
- cerr << "\n kesz vagyok\n";
+ vector<int> components;
+ ListGraph::NodeIt s(g);
+ int max = 0;
+ do{
+ 	reached = 1;
+ 	dfs.run(s);
+	visited[s]=true;
+	for (ListGraph::NodeIt i(g); unreached > 0 && i != INVALID; ++i){
+		if( dfs.reached(i) && !visited[i]) {
+			reached ++;
+			visited[i] = true;
+		}
+	 }
+	for (ListGraph::NodeIt i(g); visited[s] && i != INVALID; ++i)
+		s = i;
+	unreached -= reached;
+	components.push_back(reached);
+	max = ( max < reached)? reached:max;
+	cerr << "\n\t" << reached << " nodes reached,\t"<< unreached <<" nodes to go\t";
+ } while( unreached > 0 && !visited[s] );
+ SumNodes = countNodes(g);
+ cerr << "\nTotal number of nodes: \t\t\t" << SumNodes << endl;
+ cerr << "\nI just found " << components.size() << " components in the graph, and the biggest one has " << max << " nodes\n";
+ cerr << "\n all done\n";
  return 0;
 }

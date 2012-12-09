abfb086f3fe9a54c6271f5cb490e16e5d758f9f0
Norbert Sana <neilus@padawan.icss.loc>
2012-12-09 17:14:26 +0100

    a gyakorlo.cpp-ben jonak tuno szintezo algoritmust implementaltam

--- gyakorlo.cpp
+++ gyakorlo.cpp
@@ -7,56 +7,59 @@
 #include <lemon/lgf_reader.h>
 #include <lemon/bfs.h>
 #include <lemon/edge_set.h>
-
+#include <stdlib.h>
 
 using namespace lemon;
 using namespace std;
-/*** 
-	Itt akarom az adott elet megforditani a grafban
-*/
-
 
 
 void szintezo(ListDigraph &G, const ListDigraph::NodeMap<int> &d , const ListDigraph::NodeMap<int> &label){
 	ListDigraph::NodeMap<int> l(G);
 	ListDigraph::NodeMap<int> ro(G);
+	ListDigraph::NodeIt hungry(G);
 	const int N = countNodes(G);
-	cerr <<"INFO:\t" << "label[i]\t" << "ro[i]\t" <<"d[i]" <<endl;
+	int maxL = 0;
+
+	cerr <<"INFO:\tlabel[i]\tro[i]\td[i]\tl[i]" << endl;
+	
 	for( ListDigraph::NodeIt i(G);i!=INVALID;++i){
 		l[i]=0;
 		ro[i] = countInArcs(G,i);
-		cerr <<"INFO:\t" << label[i] <<"\t\t"<< ro[i]<<"\t" <<d[i] <<endl;
+		if(ro[i] < d[i])
+			hungry = i;
+		cerr <<"INFO:\t"<< label[i] <<"\t\t"<< ro[i]<<"\t" <<d[i] << "\t"<< l[i]<<endl;
 	}
-	
-	bool itis=true; /// van-e olyan csucs amit meg forgatnank?
-	bool isNth=false; /// felert-e mar valamelyik csucs az N. szintre?
+	if(ro[hungry] >= d[hungry])
+		hungry = INVALID;
+
 	ListArcSet<ListDigraph> arcs(G);
 	cout << "\tN is:" << N <<endl;
 	
-	while( itis && !isNth ){
-		itis=false;
+	
+	while( maxL < N && hungry != INVALID ){
+		cerr <<"INFO:\tlabel[i]\tro[i]\td[i]\tl[i]" << endl;
 		for(ListDigraph::NodeIt i(G); i!=INVALID; ++i){
-			cerr << "DEBUG:\t arcs of "  <<label[i] << " to nodes: ";
+			
 			for (ListDigraph::OutArcIt a(G,i); a!= INVALID ; ++a)
 				{
-					cerr <<"\t" << label[G.target(a)];
-					//megtalaltam a rohadekot, most ha jo csucsba vezet akkor meg is kene forditani...
-					G.reverseArc(a);
+					//cerr <<"\t" << label[G.target(a)];
+					if( (ro[i] < d[i]) && ( abs( l[G.target(a)] - l[i] ) <= 1  ) ){  
+					/// ha i ehes meg es a szomszedja nincs 1-nel messzebb akkor cserelhetnek elet
+						//megtalaltam a rohadekot, most ha jo csucsba vezet akkor meg is kene forditani...
+						cerr << "\tDEBUG:" << label[i] << " es " << label[G.target(a)] << "kozott elet forditok" <<endl;
+						G.reverseArc(a); /// megforditom az elet, ezert ...
+						ro[i] ++; /// most bele egyel tobb el mutat
+						ro[G.target(a)]--; /// bele pedig egyel kevesebb
+					}
+					else{
+						l[i] ++; /// ha nincs megfelelo szomszedja, akkor szintet lephet
+						if(maxL < l[i]) /// ha o van most a legmagasabban akkor megjegyezzuk a szintet amin van
+							maxL = l[i];
+					}
 
 				}
-			cerr <<endl;
-			if(ro[i] < d[i]){	
-
-				
-				// ConArcIt<Graph> e(G);			
-				// elforditas(G,e);
-				ro[i] +=1;
-				l[i]  +=1;
-				itis = itis || ( ro[i] < d[i] );
-				isNth =  ( l[i] >=  N );
-				//cerr << "DEBUG szintezo:\t" << label[i] << "nek elet akarunk forditani, magassaga:"<<isNth<<"\t"<<l[i]<<endl;
-			}
-			
+			cerr <<"DEBUG:\t" << label[i] <<"\t\t"<< ro[i]<<"\t" <<d[i] << "\t"<< l[i]<<endl;
+						
 		}
 		
 
